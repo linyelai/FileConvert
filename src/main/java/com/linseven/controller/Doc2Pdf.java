3543b698f978e77161cd643767d46acffb0583eb
unknown <linyelai123@163.com>
2020-07-09 17:18:56 +0800

    aspose 18.8

--- src/main/java/com/linseven/controller/Doc2Pdf.java
+++ src/main/java/com/linseven/controller/Doc2Pdf.java
@@ -1,9 +1,8 @@
 package com.linseven.controller;
-import com.aspose.cells.Workbook;
+
 import com.aspose.words.Document;
 import com.aspose.words.License;
 import com.aspose.words.SaveFormat;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -16,14 +15,15 @@ public class Doc2Pdf {
 
     public  boolean getLicense() {
         boolean result = false;
-      /*  try {
-            InputStream is =new FileInputStream("d:/license.xml"); // license.xml应放在…\WebRoot\WEB-INF\classes路径下
+        try {
+            ClassLoader classLoader = Doc2Pdf.class.getClassLoader();
+            InputStream is =classLoader.getResourceAsStream("license.xml"); // license.xml应放在…\WebRoot\WEB-INF\classes路径下
             License aposeLic = new License();
             aposeLic.setLicense(is);
             result = true;
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
         return result;
     }
 
@@ -34,7 +34,7 @@ public class Doc2Pdf {
         }
         try {
             long old = System.currentTimeMillis();
-            File file = new File("pdf1.pdf"); //新建一个空白pdf文档
+            File file = new File("d:/pdf1.pdf"); //新建一个空白pdf文档
             FileOutputStream os = new FileOutputStream(file);
             Document doc = new Document(Address); //Address是将要被转化的word文档
             doc.save(os, SaveFormat.PDF);//全面支持DOC, DOCX, OOXML, RTF HTML, OpenDocument, PDF, EPUB, XPS, SWF 相互转换
@@ -43,23 +43,8 @@ public class Doc2Pdf {
             e.printStackTrace();
         }
     }
-    public  void excel2pdf(String Address) {
-
-        if (!getLicense()) {          // 验证License 若不验证则转化出的pdf文档会有水印产生
-            return;
-        }
-        try {
-            File pdfFile = new File("d:/pdf1.pdf");// 输出路径
-            Workbook wb = new Workbook(Address);// 原始excel路径
-            FileOutputStream fileOS = new FileOutputStream(pdfFile);
-            wb.save(fileOS, SaveFormat.PDF);
-            fileOS.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     public static  void main(String [] args){
         Doc2Pdf doc2Pdf = new Doc2Pdf();
-        doc2Pdf.excel2pdf("d:/index.xlsx");
+        doc2Pdf.doc2pdf("d:/images/Word.docx");
     }
 }
